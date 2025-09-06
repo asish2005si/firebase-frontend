739aefd376694253fd6fe9c37e01fdb27a078c35
Asish Kumar Padhy <asishkumarpadhy123@gmail.com>
2025-09-06 21:43:45 +0000

    quic link remove

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 
 import { Header } from "@/components/dashboard/header";
-import { Sidebar } from "@/components/dashboard/sidebar";
 import { ClientOnly } from "@/components/client-only";
 
 export default function DashboardLayout({
@@ -12,9 +11,6 @@ export default function DashboardLayout({
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Header />
       <div className="flex flex-1">
-        <ClientOnly>
-            <Sidebar />
-        </ClientOnly>
         <main className="flex-1 p-4 sm:p-6 md:p-8">{children}</main>
       </div>
     </div>

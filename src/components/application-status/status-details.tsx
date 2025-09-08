1ae11ae4fe18f8f6aa9f107c63618a64b7244f77
Asish Kumar Padhy <asishkumarpadhy123@gmail.com>
2025-09-08 11:28:33 +0000

    Initial Deposit
    ₹500.00  replace india currency

--- src/components/application-status/status-details.tsx
+++ src/components/application-status/status-details.tsx
@@ -21,6 +21,7 @@ const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat('en-IN', {
       style: 'currency',
       currency: 'INR',
+      minimumFractionDigits: 0,
     }).format(amount);
 };
 
